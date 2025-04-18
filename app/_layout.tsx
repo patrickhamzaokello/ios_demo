6a6e10acd73e392b98b70b34be0b40f42f387226
Pkasemer <41443166+patrickhamzaokello@users.noreply.github.com>
2025-04-18 17:39:05 +0300

    update on icons and splash screen config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,61 @@
 import { StyleSheet, Text, View } from "react-native"
-import React from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { Slot, Stack } from "expo-router"
 import { AuthProvider } from "@/contexts/authContext"
 import { PlayerProvider } from "@/providers/PlayerProvider";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 400,
+  fade: true,
+});
+
 
 const StackLayout = () => {
   return <Stack screenOptions={{ headerShown: false }}></Stack>
 };
 
 export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hide();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
 
@@ -20,6 +66,5 @@ export default function RootLayout() {
       </AuthProvider>
 
     </GestureHandlerRootView>
-  )
+  );
 }
-const sytles = StyleSheet.create({})
